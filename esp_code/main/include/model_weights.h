27dd6b40c4e1782939ef6acdc3922e18ff272faf
HOANGNAM1807 <dangcongnam2004@gmail.com>
2025-01-17 00:30:08 +0700

    deploy

---
+++ esp_code/main/include/model_weights.h
@@ -0,0 +1,18 @@
+#ifndef MODEL_WEIGHTS_H
+#define MODEL_WEIGHTS_H
+// Định nghĩa weights và bias
+// extern float weight_conv1_weight[];
+// extern float weight_conv1_bias[];
+
+// extern float weight_conv2_weight[];
+// extern float weight_conv2_bias[];
+
+// extern float weight_fc1_weight[];
+// extern float weight_fc1_bias[];
+
+// extern float weight_fc2_weight[]; 
+// extern float weight_fc2_bias[];
+
+// extern float weight_fc3_weight[]; 
+// extern float weight_fc3_bias[];
+#endif
